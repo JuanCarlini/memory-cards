304a43654f928c998a11ecebdba6a763dcc78116
ramagohlke <146681360+ramagohlke@users.noreply.github.com>
2026-07-22 18:30:08 -0300

    agregar validaciones de nombre alfanumerico, mail y mensaje para contacto

--- js/validations.js
+++ js/validations.js
@@ -3,6 +3,9 @@
 var Validaciones = (function () {
 
   var LARGO_MINIMO_NOMBRE = 3;
+  var LARGO_MINIMO_MENSAJE = 5;
+  var REGEX_ALFANUMERICO = /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s]+$/;
+  var REGEX_MAIL = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   function validarNombre(nombre) {
     if (nombre.length < LARGO_MINIMO_NOMBRE) {
@@ -11,8 +14,29 @@ var Validaciones = (function () {
     return true;
   }
 
+  function validarNombreContacto(nombre) {
+    if (nombre.length === 0) {
+      return false;
+    }
+    return REGEX_ALFANUMERICO.test(nombre);
+  }
+
+  function validarMail(mail) {
+    return REGEX_MAIL.test(mail);
+  }
+
+  function validarMensaje(mensaje) {
+    if (mensaje.length <= LARGO_MINIMO_MENSAJE) {
+      return false;
+    }
+    return true;
+  }
+
   return {
-    validarNombre: validarNombre
+    validarNombre: validarNombre,
+    validarNombreContacto: validarNombreContacto,
+    validarMail: validarMail,
+    validarMensaje: validarMensaje
   };
 
 })();
